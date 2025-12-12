1d760c80edd595c11658a562c2e930308efc0321
rommuluslewis <rommuluslewis@gmail.com>
2025-12-13 04:05:56 +0800

    nfw+sersic pos+flux run and setup for systme 3 (good ell)

--- Lensing/Simulations/Input/Neirenberg/nfw+sersic+pos+flux.py
+++ Lensing/Simulations/Input/Neirenberg/nfw+sersic+pos+flux.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import glafic
+import os
 
-glafic.init(0.3, 0.7, -1.0, 0.7, '/home/rommulus/Projects/itng_lensing/Simulations/Input/Neirenberg/source_plane/NFW+SERSIC+POS+FLUX', -3.5, -3.5, 3.5, 3.5, 0.01, 0.01, 1, verb = 0)
+os.chdir('/Users/ainsleylewis/Documents/Astronomy/HPC/Lensing/Simulations/Input/Neirenberg')
+
+glafic.init(0.3, 0.7, -1.0, 0.7, 'image_plane/NFW+SERS+POSFLUX', -3.5, -3.5, 3.5, 3.5, 0.01, 0.01, 1, verb = 0)
 glafic.set_secondary('chi2_splane 0', verb = 0)
 glafic.set_secondary('chi2_checknimg 0', verb = 0)
 glafic.set_secondary('chi2_restart   -1', verb = 0)
@@ -10,9 +13,9 @@ glafic.set_secondary('hvary          0', verb = 0)
 glafic.set_secondary('ran_seed -122000', verb = 0)
 
 glafic.startup_setnum(2, 0, 1)
-glafic.set_lens(1, 'anfw', 0.2900, 7.627179e+11, 3.409167e-02, -1.322709e-02, 9.034902e-03, -5.274117e+01, 4.464198e+01, 0.000000e+00)
+glafic.set_lens(1, 'anfw', 0.2900, 3.698924e+11,  1.404833e-02, -1.323720e-02,  1.636529e-01, -3.081125e+01,  9.025613e+01,  0.000000e+00)
 glafic.set_lens(2, 'sers', 0.2900, 9.055939e+09, -1.639984e-03,  3.946928e-03,  8.150890e-02,  9.536450e+00,  6.702000e-01,  2.224560e+00)
-glafic.set_point(1, 1.7130, 3.124038e-02, -1.107056e-02)
+glafic.set_point(1, 1.7130, 2.441747e-02, -5.361933e-04)
 
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
 glafic.setopt_lens(2, 0, 1, 1, 1, 0, 0, 0, 0)
@@ -20,8 +23,8 @@ glafic.setopt_point(1, 0, 1, 1)
 
 glafic.model_init(verb = 0)
 
-glafic.readobs_point('/home/rommulus/Projects/itng_lensing/Simulations/Input/Neirenberg/Eobs_fluxpoint.dat')
-glafic.parprior('/home/rommulus/Projects/itng_lensing/Simulations/Input/Neirenberg/prior_NFW_gau_G.dat')
+glafic.readobs_point('Eobs_fluxpoint.dat')
+glafic.parprior('prior_NFW_gau_G.dat')
 glafic.optimize()
 glafic.findimg()
 glafic.writecrit(1.7130)
