c8ea5f82c910af3e0461c3d0ed56f4516bd1fc72
rommuluslewis <rommuluslewis@gmail.com>
2025-12-13 12:50:55 +0800

    Systme 3 1st Gen SIE 4 Model Run

--- Lensing/Code/sim_1.py
+++ Lensing/Code/sim_1.py
@@ -245,8 +245,8 @@ def run_glafic_calculation(params, model_name, worker_temp_dir):
     m_val, n_val, o_val = params
     output_path = os.path.join(worker_temp_dir, model_name)
     
-    base_lens_params = [0.261343256161012, 1e11, 0.0, 0.0, 0.2, 0.0, 80, 0.0]
-    base_shear_params = [0.261343256161012, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    base_lens_params = [0.297717684517447, 160, 1.06, 1.06, 0.1, 55, 0.0, 0.0]
+    base_shear_params = [0.297717684517447, 1.0, 1.06, 1.06, 0.0, 0.0, 0.0, 0.0]
 
     current_lens_params = list(base_lens_params)
     current_shear_params = list(base_shear_params)
@@ -254,7 +254,7 @@ def run_glafic_calculation(params, model_name, worker_temp_dir):
     current_shear_params[o_param - 1] = o_val
     current_shear_params[m_param - 1] = m_val
 
-    glafic.init(0.3089901684739047, 0.6910098315260953, -1.0, 0.6736, output_path, -3.0, -3.0, 3.0, 3.0, 0.01, 0.01, 1, verb=0)
+    glafic.init(0.3089901684739047, 0.6910098315260953, -1.0, 0.6736, output_path, 0.0, 0.0, 2.12, 2.12, 0.01, 0.01, 1, verb=0)
     glafic.set_secondary('chi2_splane 1', verb=0)
     glafic.set_secondary('chi2_checknimg 0', verb=0)
     glafic.set_secondary('chi2_restart   -1', verb=0)
@@ -262,11 +262,11 @@ def run_glafic_calculation(params, model_name, worker_temp_dir):
     glafic.set_secondary('hvary          0', verb=0)
     glafic.set_secondary('ran_seed -122000', verb=0)
     glafic.startup_setnum(2, 0, 1)
-    glafic.set_lens(1, 'anfw', *current_lens_params)
+    glafic.set_lens(1, 'sie', *current_lens_params)
     glafic.set_lens(2, 'pert', *current_shear_params)
-    glafic.set_point(1, 1.0, 0.0, 0.0)
-    glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 1, 0)
-    glafic.setopt_lens(2, 0, 0, 0, 0, 1, 1, 0, 1)
+    glafic.set_point(1, 1.0, 1.06, 1.06)
+    glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
+    glafic.setopt_lens(2, 0, 0, 0, 0, 0, 0, 0, 0)
     glafic.setopt_point(1, 0, 1, 1)
     glafic.model_init(verb=0)
     glafic.readobs_point(constraint_file)
--- Lensing/Simulations/Input/System_3/sie_input.py
+++ Lensing/Simulations/Input/System_3/sie_input.py
@@ -1,11 +1,11 @@
 import glafic
 
-path = '/Volumes/T7 Shield/Simulations/Output/SIE_SHEAR_0.001_360.0_1.0'
-constraint_file = '/Volumes/T7 Shield/Simulations/Input/pos_point.dat'
+path = '/Users/ainsleylewis/Documents/Astronomy/HPC/Lensing/Simulations/Input/System_3/test'
+constraint_file = '/Users/ainsleylewis/Documents/Astronomy/HPC/Lensing/Simulations/Input/System_3/pos_point.dat'
 prior_file = None
 critical_curve = False
 
-glafic.init(0.3, 0.7, -1.0, 0.7, path, -10.0, -10.0, 12.12, 12.12, 0.01, 0.01, 1, verb=0)
+glafic.init(0.3089901684739047, 0.6910098315260953, -1.0, 0.6736, path, 0.0, 0.0, 2.12, 2.12, 0.01, 0.01, 1, verb=0)
 glafic.set_secondary('chi2_splane 1', verb=0)
 glafic.set_secondary('chi2_checknimg 0', verb=0)
 glafic.set_secondary('chi2_restart   -1', verb=0)
@@ -13,11 +13,11 @@ glafic.set_secondary('chi2_usemag    1', verb=0)
 glafic.set_secondary('hvary          0', verb=0)
 glafic.set_secondary('ran_seed -122000', verb=0)
 glafic.startup_setnum(1, 0, 1)
-glafic.set_lens(1, 'sie', 0.261343256161012, 1.30e+02, 0.0, 0.0, 0.107, 23.38, 0.0, 0.0)
-glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.001, 360.0, 0.0, 1.0)
-glafic.set_point(1, 1.0, 0.0, 0.0)
+glafic.set_lens(1, 'sie', 0.297717684517447, 160, 1.06, 1.06, 0.1, 55, 0.0, 0.0)
+# glafic.set_lens(2, 'pert', 0.297717684517447, 1.0, 1.06, 1.06, 0.001, 360.0, 0.0, 0.0)
+glafic.set_point(1, 1.0, 1.06, 1.06)
 glafic.setopt_lens(1, 0, 1, 1, 1, 1, 1, 0, 0)
-glafic.setopt_lens(2, 0, 0, 0, 0, 1, 1, 0, 1)
+# glafic.setopt_lens(2, 0, 0, 0, 0, 1, 1, 0, 1)
 glafic.setopt_point(1, 0, 1, 1)
 glafic.model_init(verb=0)
 glafic.readobs_point(constraint_file)
