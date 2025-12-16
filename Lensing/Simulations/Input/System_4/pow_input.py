c87215873b789e0a5aff1eb1193aa1a6526e3753
rommuluslewis <rommuluslewis@gmail.com>
2025-12-17 04:09:32 +0800

    combine run + System 4 3rd Gen 4 POW Model Run

---
+++ Lensing/Simulations/Input/System_4/pow_input.py
@@ -0,0 +1,31 @@
+import glafic
+
+path = '/Users/ainsleylewis/Documents/Astronomy/HPC/Lensing/Simulations/Input/System_4/test'
+constraint_file = '/Users/ainsleylewis/Documents/Astronomy/HPC/Lensing/Simulations/Input/System_4/pos_point.dat'
+prior_file = '/Users/ainsleylewis/Documents/Astronomy/HPC/Lensing/Simulations/Input/System_4/prior.dat'
+critical_curve = False
+
+glafic.init(0.3089901684739047, 0.6910098315260953, -1.0, 0.6736, path, 0.0, 0.0, 2.12, 2.12, 0.01, 0.01, 1, verb=0)
+glafic.set_secondary('chi2_splane 1', verb=0)
+glafic.set_secondary('chi2_checknimg 0', verb=0)
+glafic.set_secondary('chi2_restart   -1', verb=0)
+glafic.set_secondary('chi2_usemag    1', verb=0)
+glafic.set_secondary('hvary          0', verb=0)
+glafic.set_secondary('ran_seed -122000', verb=0)
+glafic.startup_setnum(1, 0, 1)
+glafic.set_lens(1, 'pow', 0.297717684517447, 1.0, 1.06, 1.06, 0.1, 45, 0.5, 2.0)
+# glafic.set_lens(2, 'pert', 0.261343256161012, 1.0, 20.78, 20.78, 0.001, 360.0, 0.0, 1.0)
+glafic.set_point(1, 1.0, 0.0, 0.0)
+glafic.setopt_lens(1, 0, 0, 1, 1, 1, 1, 1, 1)
+# glafic.setopt_lens(2, 0, 0, 0, 0, 1, 1, 0, 1)
+glafic.setopt_point(1, 0, 1, 1)
+glafic.model_init(verb=0)
+glafic.readobs_point(constraint_file)
+if prior_file:
+    glafic.parprior(prior_file)
+glafic.optimize()
+glafic.findimg()
+if critical_curve:
+    glafic.writecrit(1.0)
+# glafic.writelens(1.0)
+glafic.quit()
